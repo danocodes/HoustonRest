3b25f3250945712aafed1c4493dfe9e4f4b37963
Dan Torrey <dan.torrey@reserveinteractive.com>
2018-02-28 20:35:26 -0600

    First commit

---
+++ src/main/java/com/dtorrey/HoustonRest/HoustonRestApplication.java
@@ -0,0 +1,12 @@
+package com.dtorrey.HoustonRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoustonRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoustonRestApplication.class, args);
+	}
+}
